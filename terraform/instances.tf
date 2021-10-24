96fb79178fa3ea82046d1e63d4a9d0d00272a1f2
unknown <coheneria@gmail.com>
2021-10-24 14:12:47 +0300

    adding ansible

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -7,3 +7,23 @@ resource "aws_instance" "part1-ansible-controller" {
       Name = "part1-ansible-controller"
   }
 }
+
+resource "aws_instance" "part1-web-server-1" {
+  ami = "ami-0a8e758f5e873d1c1"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet-2.id
+  key_name = "neria"
+  tags = {
+      Name = "part1-web-server-1"
+  }
+}
+
+resource "aws_instance" "part1-web-server-2" {
+  ami = "ami-0a8e758f5e873d1c1"
+  instance_type = "t2.micro"
+  subnet_id = aws_subnet.subnet-3.id
+  key_name = "neria"
+  tags = {
+      Name = "part1-web-server-2"
+  }
+}
--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -57,3 +57,56 @@ resource "aws_route_table_association" "public-route-association" {
   subnet_id      = aws_subnet.subnet-1.id
   route_table_id = aws_route_table.public-route-table.id
 }
+
+resource "aws_eip" "eip-nat" {
+  vpc      = true
+
+    tags = {
+    Name = "eip-nat"
+  }
+}
+
+resource "aws_nat_gateway" "part1-nat" {
+  allocation_id = aws_eip.eip-nat.id
+  subnet_id     = aws_subnet.subnet-1.id
+
+  tags = {
+    Name = "gw-NAT"
+  }
+}
+
+resource "aws_route_table" "private1-route-table" {
+  vpc_id       = aws_vpc.wave-finalproject-1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.part1-nat.id
+  }
+
+  tags       = {
+    Name     = "Private1 Route Table"
+  }
+}
+
+resource "aws_route_table" "private2-route-table" {
+  vpc_id       = aws_vpc.wave-finalproject-1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.part1-nat.id
+  }
+
+  tags       = {
+    Name     = "Private2 Route Table"
+  }
+}
+
+resource "aws_route_table_association" "private1-route-association" {
+  subnet_id      = aws_subnet.subnet-2.id
+  route_table_id = aws_route_table.private1-route-table.id
+}
+
+resource "aws_route_table_association" "private2-route-association" {
+  subnet_id      = aws_subnet.subnet-3.id
+  route_table_id = aws_route_table.private2-route-table.id
+}
