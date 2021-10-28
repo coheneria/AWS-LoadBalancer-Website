19a5d84b8b07a20f77dfb6896de5a660bdf5b551
unknown <coheneria@gmail.com>
2021-10-28 13:10:42 +0300

    fix terraform

--- terraform/instances.tf
+++ terraform/instances.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "part1-ansible-controller" {
   ami = "ami-0a8e758f5e873d1c1"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet-1.id
-  vpc_security_group_ids = ["aws_security_group.controller-security.id"]
+  security_groups = [aws_security_group.controller-security.id] 
   key_name = var.key
   tags = {
       Name = "part1-ansible-controller"
@@ -41,7 +41,7 @@ resource "aws_instance" "part1-web-server-1" {
   ami = "ami-0a8e758f5e873d1c1"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet-2.id
-  vpc_security_group_ids = ["aws_security_group.private-security.id"]
+  security_groups = [aws_security_group.private-security.id]
   key_name = var.key
   tags = {
       Name = "part1-web-server-1"
@@ -53,7 +53,7 @@ resource "aws_instance" "part1-web-server-2" {
   ami = "ami-0a8e758f5e873d1c1"
   instance_type = "t2.micro"
   subnet_id = aws_subnet.subnet-3.id
-  vpc_security_group_ids = ["aws_security_group.private-security.id"]
+  security_groups = [aws_security_group.private-security.id]
   key_name = var.key
   tags = {
       Name = "part1-web-server-2"
